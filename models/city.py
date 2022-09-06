6987397b0a6f937e4c63428769feedb5aaa15fe7
SebastianBlandon <blandonodz@gmail.com>
2022-09-06 08:14:40 -0500

    [UPDATED] city.py, state.py files

--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from os import getenv
 
+TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
-class City(BaseModel):
+class City(BaseModel, Base if (TYPE_STORAGE == "db") else object):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    if TYPE_STORAGE == "db":
+        __tablename__ = "cities"
+
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('state.id', nullable=False))
+    else:
+        state_id = ""
+        name = ""
--- models/state.py
+++ models/state.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String, ForeignKey
+from os import getenv
 
+TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
-class State(BaseModel):
+class State(BaseModel, Base if (TYPE_STORAGE == "db") else object):
     """ State class """
-    name = ""
+    if TYPE_STORAGE == "db":
+        __tablename__ = "states"
+
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
