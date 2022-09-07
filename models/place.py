998a4fde19c37912a7266f88284006272c58aedb
SebastianBlandon <blandonodz@gmail.com>
2022-09-06 23:32:21 -0500

    [UPDATED] place.py file task 9

--- models/place.py
+++ models/place.py
@@ -23,7 +23,7 @@ class Place(BaseModel, Base if (TYPE_STORAGE == "db") else object):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship("Review", backref="place", cascade="all, delete")
+        reviews = relationship("Review", backref="place", cascade="delete")
 
     else:
         city_id = ""
