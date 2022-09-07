faa86b94314999dabbc9928a296bd7ad4f706c67
sevajal <sevajal@yahoo.com>
2022-09-07 14:19:21 -0500

    We changed the relationship with back_populates

--- models/amenity.py
+++ models/amenity.py
@@ -13,7 +13,7 @@ class Amenity(BaseModel, Base if (TYPE_STORAGE == "db") else object):
     if TYPE_STORAGE == "db":
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary=place_amenity, viewonly=False)
+        place_amenities = relationship("Place", secondary=place_amenity, viewonly=False, back_populates="amenities")
 
     else:                   
         name = ""
