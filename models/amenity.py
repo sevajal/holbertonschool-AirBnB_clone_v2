1ef3aaad5cd699b8717e6f92f01c9952b7097767
sevajal <sevajal@yahoo.com>
2022-09-07 14:00:19 -0500

    First draft task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Amenity Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from models.place import place_amenity
+from sqlalchemy.orm import relationship
+from os import getenv
 
+TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base if (TYPE_STORAGE == "db") else object):
+    """Amenities class"""
+    if TYPE_STORAGE == "db":
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity, viewonly=False)
+
+    else:                   
+        name = ""
--- models/place.py
+++ models/place.py
@@ -2,12 +2,15 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models.review import Review
-from sqlalchemy import Column, Float, Integer, String, ForeignKey
+from sqlalchemy import Column, Float, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 import models
 
 TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
+place_amenity = Table("association", Base.metadata,
+                        Column("place_id", String(60), ForeignKey("places.id"), primary_key=True),
+                        Column("amenity_id", String (60), ForeignKey("amenities.id"), primary_key=True),)
 
 class Place(BaseModel, Base if (TYPE_STORAGE == "db") else object):
     """ A place to stay """
@@ -24,6 +27,7 @@ class Place(BaseModel, Base if (TYPE_STORAGE == "db") else object):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         reviews = relationship("Review", backref="place", cascade="all, delete")
+        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
 
     else:
         city_id = ""
@@ -46,4 +50,18 @@ class Place(BaseModel, Base if (TYPE_STORAGE == "db") else object):
                 if review.place_id == self.id:
                     review_list.append(review)
             return review_list
-        
+
+        @property
+        def amenities(self):
+            """gets a list of amenities linked to the place"""
+            list_amenities = []
+            for amenity in models.storage.all(Amenity).values(): 
+                if amenity.id in self.amenity_ids:
+                    list_amenities.append(amenity)
+            return list_amenities
+
+        @amenities.setter
+        def amenities(self, new_amenity):
+            """append new amenities ids to amenity_ids"""
+            if type(new_amenity) == Amenity:
+                self.amenity_ids.append(new_amenity.id)
