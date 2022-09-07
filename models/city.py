184daa8c925ffacae33e1f4abecd7564122786ca
sevajal <sevajal@yahoo.com>
2022-09-07 11:41:28 -0500

    We fixed the relacionship

--- models/city.py
+++ models/city.py
@@ -13,7 +13,7 @@ class City(BaseModel, Base if (TYPE_STORAGE == "db") else object):
         __tablename__ = "cities"
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", backref="cities", cascade="delete")
+        places = relationship("Place", backref="cities", cascade="all, delete")
     else:
         name = ""
         state_id = ""
