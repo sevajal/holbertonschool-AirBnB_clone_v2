162f5d8a49aa7a678e3984457e6d127336aa28a3
sevajal <sevajal@yahoo.com>
2022-09-07 15:08:58 -0500

    We deleted the relationship

--- models/amenity.py
+++ models/amenity.py
@@ -2,7 +2,6 @@
 """ Amenity Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from models.place import place_amenity
 from sqlalchemy.orm import relationship
 from os import getenv
 
@@ -13,7 +12,5 @@ class Amenity(BaseModel, Base if (TYPE_STORAGE == "db") else object):
     if TYPE_STORAGE == "db":
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary=place_amenity, viewonly=False, back_populates="amenities")
-
     else:                   
         name = ""
