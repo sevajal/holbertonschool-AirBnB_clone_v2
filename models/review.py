cd09f631bb41c3244134edcc6e1da5b0a7ebf85a
sevajal <sevajal@yahoo.com>
2022-09-07 11:46:02 -0500

    We changed the foreign keys

--- models/review.py
+++ models/review.py
@@ -11,8 +11,8 @@ class Review(BaseModel, Base if (TYPE_STORAGE == "db") else object):
     if TYPE_STORAGE == "db":
         __tablename__ = "reviews"
         text = Column(String(1024), nullable=False)
-        user_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        place_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
 
     else:
         place_id = ""
