0fbe65ba5a085cf35bfed0647cf2741c41f77a7a
sevajal <sevajal@yahoo.com>
2022-09-07 11:22:36 -0500

    We changed the table position

--- models/user.py
+++ models/user.py
@@ -9,8 +9,8 @@ TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
 class User(BaseModel, Base if (TYPE_STORAGE == "db") else object):
     """This class defines a user by various attributes"""
+    __tablename__ = "users"
     if TYPE_STORAGE == "db":
-        __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
