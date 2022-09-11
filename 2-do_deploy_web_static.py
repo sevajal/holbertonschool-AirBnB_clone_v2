108118c6f705a898b76f9c32fd6307d0897d35cd
sevajal <sevajal@yahoo.com>
2022-09-10 23:12:42 -0500

    _i fixed style

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,7 @@ env.hosts = ['web-01.sevajal.tech', 'web-02.sevajal.tech']
 env.user = 'ubuntu'
 env.key = '~/.ssh/school'
 
+
 def do_deploy(archive_path):
     '''Deploy the archive in the web servers'''
     if not os.path.exists(archive_path):
@@ -17,12 +18,15 @@ def do_deploy(archive_path):
     try:
         put(archive_path, '/tmp/{}'.format(file))
         run('mkdir -p /data/web_static/releases/{}/'.format(name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(file, name))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'
+            .format(file, name))
         run('rm /tmp/{}'.format(file))
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(name, name))
+        run('mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/'.format(name, name))
         run('rm -rf /data/web_static/releases/{}/web_static'.format(name))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(name))
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(name))
         return (True)
     except Exception as error:
         return (False)
