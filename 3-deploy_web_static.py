c797c6079be02e64856672d6d0cf7e11a9b1382a
sevajal <sevajal@yahoo.com>
2022-09-11 21:27:39 -0500

    I put the functions inside

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,13 +1,47 @@
 #!/usr/bin/python3
 '''Fabric script that creates and distributes an archive to my web servers,
 using the function deploy.'''
-from fabric.api import env
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
+from fabric.api import env, local, put, run
+import os
 
 env.hosts = ['web-01.sevajal.tech', 'web-02.sevajal.tech']
 env.user = 'ubuntu'
 env.key = '~/.ssh/school'
+
+
+def do_pack():
+    '''pack the files in the folder'''
+    timestr = time.strftime("%Y%m%d%H%M%S")
+    file = "web_static_" + timestr + ".tgz"
+    try:
+        local('mkdir -p versions')
+        local('tar -cvzf versions/{} web_static'.format(file))
+        return ('versions/{}'.format(file))
+    except Exception as error:
+        return (None)
+
+
+def do_deploy(archive_path):
+    '''Deploy the archive in the web servers'''
+    if not os.path.exists(archive_path):
+        return (False)
+    file = archive_path.split('/')[1]
+    name = file.split('.')[0]
+    try:
+        put(archive_path, '/tmp/{}'.format(file))
+        run('mkdir -p /data/web_static/releases/{}/'.format(name))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'
+            .format(file, name))
+        run('rm /tmp/{}'.format(file))
+        run('mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/'.format(name, name))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(name))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(name))
+        return (True)
+    except Exception as error:
+        return (False)
 
 
 def deploy():
