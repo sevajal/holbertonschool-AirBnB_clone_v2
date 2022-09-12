911b989afb1abf8ac3f1f7fb3aef7e0eda053b6d
sevajal <sevajal@yahoo.com>
2022-09-11 21:15:03 -0500

    I did the task 3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Fabric script that creates and distributes an archive to my web servers,
+using the function deploy.'''
+from fabric.api import env
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+env.hosts = ['web-01.sevajal.tech', 'web-02.sevajal.tech']
+env.user = 'ubuntu'
+env.key = '~/.ssh/school'
+
+
+def deploy():
+    '''Creates and deploy an archive to the web servers'''
+    archive_path = do_pack()
+    return (do_deploy(archive_path))
