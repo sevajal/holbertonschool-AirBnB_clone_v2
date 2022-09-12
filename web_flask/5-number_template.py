3abe4f2f82e414f54aebba3c2b071fce290d4236
sevajal <sevajal@yahoo.com>
2022-09-12 12:40:04 -0500

    I did the task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+'''Script that starts a Flask web application'''
+
+from flask import Flask
+from flask import render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    '''Index route'''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''/hbnb route'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def print_c_text(text):
+    '''/c/<text> route'''
+    text = text.replace("_", " ")
+    return f'C {escape(text)}'
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def print_python_text(text='is cool'):
+    '''/python/<text> route'''
+    return f'Python {escape(text)}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def print_number(n):
+    '''/number/<n> route'''
+    return f'{escape(n)} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    '''/number_template/<n> route'''
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
