3db6902615c125fcf5fdb006ca7d17061f1d416a
sevajal <sevajal@yahoo.com>
2022-09-12 11:39:28 -0500

    I did the task 1 and fixed style of task 0

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,5 +12,11 @@ def index():
     return "Hello HBNB!"
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''/hbnb route'''
+    return 'HBNB'
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
