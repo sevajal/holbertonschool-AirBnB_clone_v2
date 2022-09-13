44ff9819147ff981c89bb19d8ad592c8c4050c9a
sevajal <sevajal@yahoo.com>
2022-09-13 11:56:39 -0500

    First draft task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''Script that starts a Flask web application'''
+
+from flask import Flask
+from flask import render_template
+from markupsafe import escape
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session(self):
+    '''Remove the current SQLAlchemy Session'''
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+def display_states():
+    '''Display a HTML page with the states'''
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def display_cities_of_a_state():
+    '''Display a HTML page with the states'''
+    states = storage.all(State).values()
+    state_name = ""
+    state_info = []
+    for state in states:
+        if state.id == id:
+            state_name = state.name
+            state_info.append(state)
+    return render_template('9-states.html', id=id, state_info=state_info, state_name=state_name)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
